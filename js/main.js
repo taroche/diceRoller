3ac63e587d47f3071f26b3be19323c54b69fd80e
Tristan <laroche11235@gmail.com>
2019-08-28 09:38:48 -0400

    cleaned up my code

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
-
-const fourSide = function(){
-    let dice = document.getElementById('fourSideD');
-    let count = document.getElementById('countFourD');
-    let result = document.getElementById('resultFourD');
-    const minus = document.getElementById('minusFourD');
-    const plus = document.getElementById('plusFourD');
-
-
-    minus.onclick = function() {
-        count.stepDown(1);
-    };
-    plus.onclick = function() {
-        count.stepUp(1);
-    };
-        roll = [];
-    dice.onclick = () => {
-        for (let i = 0; i < count.value; i++) {
-            roll.push( Math.floor(Math.random() * 4) + 1);
-        }
-        let rollAsString = roll.join(', ')
-        if (roll.length <= 20){
-            result.innerHTML = rollAsString;
-        }
-        else {
-            result.innerHTML = 'You are trying to roll to many Dice.';
-        }
-        roll.length = 0;
-    }
-}();
-const sixSide = function(){
-    let dice = document.getElementById('sixSideD');
-    let count = document.getElementById('countSixD');
-    let result = document.getElementById('resultSixD');
-    const minus = document.getElementById('minusSixD');
-    const plus = document.getElementById('plusSixD');
-
+const running = (dice, count, result, minus, plus) => {
     minus.onclick = function() {
         count.stepDown(1);
     };
@@ -55,6 +19,24 @@ const sixSide = function(){
         }
         roll.length = 0;
     }
+}
+const fourSide = function(){
+    let dice = document.getElementById('fourSideD');
+    let count = document.getElementById('countFourD');
+    let result = document.getElementById('resultFourD');
+    const minus = document.getElementById('minusFourD');
+    const plus = document.getElementById('plusFourD');
+
+    running(dice, count, result, minus, plus)
+}();
+const sixSide = function(){
+    let dice = document.getElementById('sixSideD');
+    let count = document.getElementById('countSixD');
+    let result = document.getElementById('resultSixD');
+    const minus = document.getElementById('minusSixD');
+    const plus = document.getElementById('plusSixD');
+
+    running(dice, count, result, minus, plus)
 }();
 const eightSide = function(){
     let dice = document.getElementById('eightSideD');
@@ -63,26 +45,7 @@ const eightSide = function(){
     const minus = document.getElementById('minusEightD');
     const plus = document.getElementById('plusEightD');
 
-    minus.onclick = function() {
-        count.stepDown(1);
-    };
-    plus.onclick = function() {
-        count.stepUp(1);
-    };
-        roll = [];
-    dice.onclick = () => {
-        for (let i = 0; i < count.value; i++) {
-            roll.push(Math.floor(Math.random() * 8) + 1);
-        }
-        let rollAsString = roll.join(', ')
-        if (roll.length <= 20){
-            result.innerHTML = rollAsString
-        }
-        else {
-            result.innerHTML = 'You are trying to roll to many Dice.'
-        }
-        roll.length = 0;
-    }
+    running(dice, count, result, minus, plus)
 }();
 const tenSide = function(){
     let dice = document.getElementById('tenSideD');
@@ -91,26 +54,7 @@ const tenSide = function(){
     const minus = document.getElementById('minusTenD');
     const plus = document.getElementById('plusTenD');
 
-    minus.onclick = function() {
-        count.stepDown(1);
-    };
-    plus.onclick = function() {
-        count.stepUp(1);
-    };
-        roll = [];
-    dice.onclick = () => {
-        for (let i = 0; i < count.value; i++) {
-            roll.push(Math.floor(Math.random() * 10) + 1);
-        }
-        let rollAsString = roll.join(', ')
-        if (roll.length <= 20){
-            result.innerHTML = rollAsString
-        }
-        else {
-            result.innerHTML = 'You are trying to roll to many Dice.'
-        }
-        roll.length = 0;
-    }
+    running(dice, count, result, minus, plus)
 }();
 const twelveSide = function(){
     let dice = document.getElementById('twelveSideD');
@@ -119,26 +63,7 @@ const twelveSide = function(){
     const minus = document.getElementById('minusTwelveD');
     const plus = document.getElementById('plusTwelveD');
 
-    minus.onclick = function() {
-        count.stepDown(1);
-    };
-    plus.onclick = function() {
-        count.stepUp(1);
-    };
-        roll = [];
-    dice.onclick = () => {
-        for (let i = 0; i < count.value; i++) {
-            roll.push(Math.floor(Math.random() * 12) + 1);
-        }
-        let rollAsString = roll.join(', ')
-        if (roll.length <= 20){
-            result.innerHTML = rollAsString
-        }
-        else {
-            result.innerHTML = 'You are trying to roll to many Dice.'
-        }
-        roll.length = 0;
-    }
+    running(dice, count, result, minus, plus)
 }();
 const twentySide = function(){
     let dice = document.getElementById('twentySideD');
