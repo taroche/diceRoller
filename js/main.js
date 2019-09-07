732d94b9aad72bb8b61ed27b82230fc17f88cf70
Tristan <laroche11235@gmail.com>
2019-09-07 12:06:55 -0400

    fix bug where they were all rolling six

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const running = (dice, count, result, minus, plus) => {
+
+const running = (dice, count, result, minus, plus, number) => {
     minus.onclick = function() {
         count.stepDown(1);
     };
@@ -8,7 +9,7 @@ const running = (dice, count, result, minus, plus) => {
         roll = [];
     dice.onclick = () => {
         for (let i = 0; i < count.value; i++) {
-            roll.push(Math.floor(Math.random() * 6) + 1);
+            roll.push(Math.floor(Math.random() * number) + 1);
         }
         let rollAsString = roll.join(', ')
         if (roll.length <= 20){
@@ -19,52 +20,63 @@ const running = (dice, count, result, minus, plus) => {
         }
         roll.length = 0;
     }
-}
+};
+
 const fourSide = function(){
     let dice = document.getElementById('fourSideD');
     let count = document.getElementById('countFourD');
     let result = document.getElementById('resultFourD');
     const minus = document.getElementById('minusFourD');
     const plus = document.getElementById('plusFourD');
+    const number = 4;
 
-    running(dice, count, result, minus, plus)
+    running(dice, count, result, minus, plus, number)
 }();
+
 const sixSide = function(){
     let dice = document.getElementById('sixSideD');
     let count = document.getElementById('countSixD');
     let result = document.getElementById('resultSixD');
     const minus = document.getElementById('minusSixD');
     const plus = document.getElementById('plusSixD');
+    const number = 6;
 
-    running(dice, count, result, minus, plus)
+    running(dice, count, result, minus, plus, number)
 }();
+
 const eightSide = function(){
     let dice = document.getElementById('eightSideD');
     let count = document.getElementById('countEightD');
     let result = document.getElementById('resultEightD');
     const minus = document.getElementById('minusEightD');
     const plus = document.getElementById('plusEightD');
+    const number = 8;
 
-    running(dice, count, result, minus, plus)
+    running(dice, count, result, minus, plus, number)
 }();
+
 const tenSide = function(){
     let dice = document.getElementById('tenSideD');
     let count = document.getElementById('countTenD');
     let result = document.getElementById('resultTenD');
     const minus = document.getElementById('minusTenD');
     const plus = document.getElementById('plusTenD');
+    const number = 10;
 
-    running(dice, count, result, minus, plus)
+    running(dice, count, result, minus, plus, number)
 }();
+
 const twelveSide = function(){
     let dice = document.getElementById('twelveSideD');
     let count = document.getElementById('countTwelveD');
     let result = document.getElementById('resultTwelveD');
     const minus = document.getElementById('minusTwelveD');
     const plus = document.getElementById('plusTwelveD');
+    const number = 12;
 
-    running(dice, count, result, minus, plus)
+    running(dice, count, result, minus, plus, number)
 }();
+
 const twentySide = function(){
     let dice = document.getElementById('twentySideD');
     let count = document.getElementById('countTwentyD');
